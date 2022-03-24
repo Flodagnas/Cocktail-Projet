659119a5d2f7f766d19b92aad3d052c01da170d0
Antoine <antoine.bouffard@ynov.com>
2022-03-24 16:47:30 +0100

    premier controller

--- app/Http/Controllers/SiropController.php
+++ app/Http/Controllers/SiropController.php
@@ -8,5 +8,27 @@ use App\Model\Sirop;
 
 class SiropController extends Controller
 {
-    //
+    public function getAllSirop(){
+        return (Sirop::all());
+    }
+
+    public function getSiropById($id) {
+        return (Sirop::where('id', $id));
+    }
+
+    public function getSiropByName($name){
+        return (Sirop::where('name', $name));
+    }
+
+    public function addSirops($name){
+        Sirop::create(['name' => $name]);
+    }
+
+    public function updateSirop($name, $newName){
+        Sirop::where('name', [$name])->update(['name' => $newName]);
+    }
+
+    public function deleteSirop($name){
+        Sirop::where('name', [$name])->delete();
+    }
 }
--- app/Http/Controllers/SoftController.php
+++ app/Http/Controllers/SoftController.php
@@ -8,5 +8,27 @@ use App\Model\Soft;
 
 class SoftController extends Controller
 {
-    //
+    public function getAllSoft(){
+        return (Soft::all());
+    }
+
+    public function getSoftById($id) {
+        return (Soft::where('id', $id));
+    }
+
+    public function getSoftByName($name){
+        return (Soft::where('name', $name));
+    }
+
+    public function addSofts($name){
+        Soft::create(['name' => $name]);
+    }
+
+    public function updateSoft($name, $newName){
+        Soft::where('name', [$name])->update(['name' => $newName]);
+    }
+
+    public function deleteSoft($name){
+        Soft::where('name', [$name])->delete();
+    }
 }
