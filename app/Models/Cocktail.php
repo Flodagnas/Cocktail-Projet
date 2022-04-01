1984aa17e6e95210cc81f61d5cfa6a6d22b554c8
Flodagnas <74758873+Flodagnas@users.noreply.github.com>
2022-04-01 15:38:11 +0200

    debug

--- app/Models/Cocktail.php
+++ app/Models/Cocktail.php
@@ -7,5 +7,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cocktail extends Model
 {
-    use HasFactory;
+    //use HasFactory;
+
+    public function getAllCocktail(){
+        $cocktails = self::all();
+        return $cocktails;
+    }
+
+    public function getCocktailById($id) {
+        return (Cocktail::where('id', $id));
+    }
+
+    public function getCocktailByName($name){
+        return (Cocktail::where('name', $name));
+    }
+
+    public function addCocktails($name){
+        Cocktail::create(['name' => $name]);
+    }
+
+    public function updateCocktail($name, $newName){
+        Cocktail::where('name', [$name])->update(['name' => $newName]);
+    }
+
+    public function deleteCocktail($name){
+        Cocktail::where('name', [$name])->delete();
+    }
+
 }
